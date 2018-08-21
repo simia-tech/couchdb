95a20485fa7932bb467ded3e8b13bb49753fcd62
Philipp Brüll <pb@simia.tech>
2018-08-21 10:49:50 +0200

    added another test

--- database_create_test.go
+++ database_create_test.go
@@ -3,6 +3,7 @@ package couchdb_test
 import (
 	"testing"
 
+	"github.com/simia-tech/errx"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -19,4 +20,17 @@ func TestDatabaseCreate(t *testing.T) {
 	assert.True(t, response.OK)
 	assert.Equal(t, "", response.Error)
 	assert.Equal(t, "", response.Reason)
+}
+
+func TestDatabaseCreateOfExitingDatabase(t *testing.T) {
+	e := setUpTestEnvironment(t)
+	defer e.tearDown()
+	db := e.client.Database("test")
+
+	_, err := db.Create().Do()
+	require.NoError(t, err)
+	defer db.Delete().Do()
+
+	_, err = db.Create().Do()
+	assert.True(t, errx.IsAlreadyExists(err))
 }
